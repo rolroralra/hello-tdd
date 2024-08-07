954ba0bdf1c59ed6b2ecc25176f914c284998160
Shinyoung Kim <rolroralra@gmail.com>
2024-08-07 14:21:56 +0900

    refactor: introduce constant

--- src/main/java/com/example/tdd/expiration/ExpirationDateCalculator.java
+++ src/main/java/com/example/tdd/expiration/ExpirationDateCalculator.java
@@ -5,13 +5,17 @@ import java.time.LocalDate;
 import java.util.Optional;
 
 public class ExpirationDateCalculator {
+
+    public static final int PAYMENT_PRICE = 10_000;
+    public static final int ONE_MONTH = 1;
+
     public LocalDate calculateExpireDate(PaymentData paymentData) {
         Preconditions.checkArgument(
-            paymentData.payAmount() >= 10_000,
-            "납부 금액은 10,000원 이상이어야 합니다."
+            paymentData.payAmount() >= PAYMENT_PRICE,
+            "납부 금액은 %s원 이상이어야 합니다.", PAYMENT_PRICE
         );
 
-        LocalDate expirationDate = paymentData.billingDate().plusMonths(1);
+        LocalDate expirationDate = paymentData.billingDate().plusMonths(ONE_MONTH);
 
         return Optional.ofNullable(paymentData.firstBillingDate())
             .map(firstBillingDate -> expirationDate.withDayOfMonth(firstBillingDate.getDayOfMonth()))
