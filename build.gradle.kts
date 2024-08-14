49aa7d134905e18f8ada7b537438eda61362dd2a
Shinyoung Kim <rolroralra@gmail.com>
2024-08-14 13:43:17 +0900

    feat: @SpringBootTest, TestRestTemplate, MockMVC, RestAssured, WireMock 예시를 위한, 기본 API 개발
    - `GET /api/v1/hello`
    - `GET /api/v1/integration`

--- build.gradle.kts
+++ build.gradle.kts
@@ -29,7 +29,15 @@ extra["snippetsDir"] = file("build/generated-snippets")
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-    implementation("org.springframework.boot:spring-boot-starter-web")
+    // Remove Tomcat dependency
+    implementation("org.springframework.boot:spring-boot-starter-web") {
+        exclude(group = "org.springframework.boot", module = "spring-boot-starter-tomcat")
+    }
+
+    // Add Jetty dependency
+    implementation("org.springframework.boot:spring-boot-starter-jetty")
+
+    implementation("org.springframework.boot:spring-boot-starter-webflux")
     implementation("org.apache.commons:commons-lang3:3.15.0")
     implementation("com.google.guava:guava:33.2.1-jre")
 
@@ -43,6 +51,16 @@ dependencies {
     testImplementation("org.springframework.boot:spring-boot-testcontainers")
     testImplementation("org.springframework.restdocs:spring-restdocs-mockmvc")
     testImplementation("org.testcontainers:junit-jupiter")
+
+    // https://www.kevinhooke.com/2024/05/03/using-wiremock-with-spring-boot-3-and-junit-4/
+    // wiremock-spring-boot 의존성 추가 및 wiremock 의존성 제외
+    testImplementation("com.maciejwalkowiak.spring:wiremock-spring-boot:2.0.0") {
+        exclude(group = "org.wiremock", module = "wiremock")
+    }
+
+    // wiremock-standalone 의존성 추가
+    testImplementation("org.wiremock:wiremock-standalone:3.3.1")
+
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
